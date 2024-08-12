c2b183ef02d7a067ec027cdbac408d5e85eec883
flokedd <floked224@gmail.com>
2024-08-12 22:09:48 +0300

    add constructor and some attributes to Ball class

--- ball.py
+++ ball.py
@@ -1,4 +1,6 @@
 import pygame
 
 class Ball:
-    pass
+    def __init__(self, color: tuple, size: tuple, pos: tuple):
+        self.color = color  
+        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
