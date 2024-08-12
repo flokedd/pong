cb7113192c4521cbc1a948c5fdf22a6a9872e4ef
flokedd <floked224@gmail.com>
2024-08-12 16:03:01 +0300

    add constructor and some attributes to Character class in character.py

--- character.py
+++ character.py
@@ -1,3 +1,7 @@
+import pygame
+
 class Character:
-    def __init__(self):
-        pass
+    def __init__(self, color: tuple, pos:list):
+        self.color = color
+        self.size = (20, 100)
+        self.rect = pygame.Rect(pos[0], pos[1], self.size[0], self.size[1])
