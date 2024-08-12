9565a10bf809543e2586ec8caf20c210b7f926a5
flokedd <floked224@gmail.com>
2024-08-12 21:10:32 +0300

    add draw method to Player class and drawboth players on the screen

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ from player import Player
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+Player.screen = screen
 running = True
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+player_one = Player(BLUE, (0, screen.get_size()[1]/2-Player.size[1]/2))
+player_two = Player(RED, (screen.get_size()[0]-Player.size[0], screen.get_size()[1]/2-Player.size[1]/2))
 
 while running:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill("black")
+    player_one.draw()
+    player_two.draw()
 
     pygame.display.flip()
 
--- player.py
+++ player.py
@@ -1,7 +1,11 @@
 import pygame
 
 class Player:
+    screen = None
+    size = (20, 100)
     def __init__(self, color: tuple, pos:tuple):
         self.color = color
-        self.size = (20, 100)
         self.rect = pygame.Rect(pos[0], pos[1], self.size[0], self.size[1])
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
