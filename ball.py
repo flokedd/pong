1bf30f83982df29dd6785a3307a12a283b8e6b09
flokedd <floked224@gmail.com>
2024-08-12 22:32:34 +0300

    add speed attribute to Ball class

--- ball.py
+++ ball.py
@@ -3,8 +3,9 @@ import pygame
 class Ball:
     screen = None
     radius = 25
-    def __init__(self, color: tuple, pos: tuple):
+    def __init__(self, color: tuple, pos: tuple, speed):
         self.color = color 
+        self.speed = speed
         self.rect = pygame.Rect(pos[0], pos[1], self.radius*2, self.radius*2)
 
     def draw(self):
