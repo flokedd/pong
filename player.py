963a1017f14eadac57dffa7904648ad330fd059b
flokedd <floked224@gmail.com>
2024-08-12 20:56:36 +0300

    change Player pos attribute from list to tuple

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Player:
-    def __init__(self, color: tuple, pos:list):
+    def __init__(self, color: tuple, pos:tuple):
         self.color = color
         self.size = (20, 100)
         self.rect = pygame.Rect(pos[0], pos[1], self.size[0], self.size[1])
